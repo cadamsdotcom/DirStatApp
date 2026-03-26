85e0996960a708f3215feb371ce4c89d5ba640ef
Chris Adams <chris@cadams.com>
2026-03-26 11:43:55 +1100

    Initial Commit

---
+++ DirStatApp/DirStatAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DirStatAppApp.swift
+//  DirStatApp
+//
+//  Created by Chris Adams on 26/3/2026.
+//
+
+import SwiftUI
+
+@main
+struct DirStatAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
